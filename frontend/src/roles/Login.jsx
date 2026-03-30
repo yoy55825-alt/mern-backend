b0c6c04449213563bf41553120b4f8546aa1aa2f
unknown <yoy55825@gmail.com>
2026-03-30 23:05:55 +0630

    Fix login for vite

--- frontend/src/roles/Login.jsx
+++ frontend/src/roles/Login.jsx
@@ -12,8 +12,8 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
-    // const API_URL = process.env.VITE_API_URL || 'http://localhost:3000';
-    const API_URL = process.env.VITE_API_URL;
+    const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+    // const API_URL = process.env.VITE_API_URL;
 
     const Login = async (e) => {
         e.preventDefault();
