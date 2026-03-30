5e07f3d1fa907379ed02da5c8c61a17f4e67317d
unknown <yoy55825@gmail.com>
2026-03-30 22:07:55 +0630

    Fix server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://mern-backend-1mbp.onrender.com"
+        "https://mern-frontend-nc6r.onrender.com"
     ],
     credentials: true
 }))
